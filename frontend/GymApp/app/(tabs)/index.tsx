e0f37d53aeb657d885e8a33c830b6c4b4011f17e
Manuka Abeysekara <it24100043@sliit.lk>
2026-04-08 20:04:32 +0530

    .

--- frontend/GymApp/app/(tabs)/index.tsx
+++ frontend/GymApp/app/(tabs)/index.tsx
@@ -1,6 +1,5 @@
 import { Redirect } from 'expo-router';
 
 export default function Index() {
-  // මේකෙන් වෙන්නේ ඇප් එක open වෙද්දීම අපි හදපු register page එකට redirect කරන එක
   return <Redirect href="/register" />;
 }
